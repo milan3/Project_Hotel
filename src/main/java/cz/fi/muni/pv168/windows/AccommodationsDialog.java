fc65677cde6039c144825c38d521f661c20543ba
Buckyx <kolotocar94@gmail.com>
2016-05-05 21:34:08 +0200

    funkcny room manager: pridavanie/mazanie/editovanie izieb + ubytovanie niekoho(random) + mazanie ubytovania

--- src/main/java/cz/fi/muni/pv168/windows/AccommodationsDialog.java
+++ src/main/java/cz/fi/muni/pv168/windows/AccommodationsDialog.java
@@ -59,6 +59,7 @@ public class AccommodationsDialog extends javax.swing.JDialog {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         comboBoxRooms = new javax.swing.JComboBox();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -87,6 +88,8 @@ public class AccommodationsDialog extends javax.swing.JDialog {
             }
         });
 
+        jButton3.setText("change accommodation");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -102,12 +105,11 @@ public class AccommodationsDialog extends javax.swing.JDialog {
                                 .addComponent(comboBoxRooms, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 306, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(105, 105, 105)
+                        .addGap(118, 118, 118)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(13, 13, 13)
-                                .addComponent(jButton1))
-                            .addComponent(jButton2))))
+                            .addComponent(jButton2)
+                            .addComponent(jButton1)
+                            .addComponent(jButton3))))
                 .addContainerGap(58, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -123,7 +125,9 @@ public class AccommodationsDialog extends javax.swing.JDialog {
                 .addComponent(jButton1)
                 .addGap(18, 18, 18)
                 .addComponent(jButton2)
-                .addContainerGap(60, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 22, Short.MAX_VALUE)
+                .addComponent(jButton3)
+                .addContainerGap())
         );
 
         pack();
@@ -217,6 +221,7 @@ public class AccommodationsDialog extends javax.swing.JDialog {
     private javax.swing.JComboBox comboBoxRooms;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tableAccommodations;
